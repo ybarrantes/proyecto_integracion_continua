83b1154cae783d4d649d314f69a1994616df5e18
Yonathan Gustavo Barrantes Leon <ybarrantes.juntos085@gmail.com>
2020-07-07 20:02:35 -0500

    fix bug string size users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,10 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique('users_email_unique');
+            $table->string('name', 60);
+            $table->string('email', 180)->unique('users_email_unique');
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password', 100);
             $table->rememberToken();
             $table->timestamps();
         });
